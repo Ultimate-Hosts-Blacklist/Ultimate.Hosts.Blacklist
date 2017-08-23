dbf01194ad2aed35d403513385ba26d56440c318
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-23 12:52:04 +0200

    Add Windows Uninstall Script (Restore hosts.bak > hosts)

---
+++ uninstall-hosts-windows.bat
@@ -0,0 +1,29 @@
+:: ULTIMATE HOSTS BLACKLIST
+:: REPO: https://github.com/mitchellkrogza/Ultimate.Hosts.Blacklist
+:: Copyright Mitchell Krog - https://github.com/mitchellkrogza
+:: This script will first create a backup of the original or current hosts
+:: file and save it in a file titled "hosts.bak" 
+:: It will then download the latest updated hosts file
+:: Finally the DNS cache will also be refreshed.
+:: THIS BAT FILE MUST BE LAUNCHED WITH ADMINISTRATOR PRIVILEGES
+:: Copyright Mitchell Krog - https://github.com/mitchellkrogza
+
+:: @ECHO OFF
+TITLE Update Hosts
+
+:: Check if we are administrator. If not, exit immediately.
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %ERRORLEVEL% NEQ 0 (
+    ECHO This script must be run with administrator privileges!
+    ECHO Please launch command prompt as administrator. Exiting...
+    EXIT /B 1
+)
+	COPY %WINDIR%\System32\drivers\etc\hosts.bak %WINDIR%\System32\drivers\etc\hosts
+)
+
+:: Flush the DNS cache
+
+ipconfig /flushdns
+
+echo ALL DONE !!! Original hosts file restored !!
