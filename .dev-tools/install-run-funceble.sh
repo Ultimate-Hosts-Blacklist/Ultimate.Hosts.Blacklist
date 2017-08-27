421a2e66d2035c4aa81c5eaf68e651b828424bf3
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-27 14:47:03 +0200

    Test domains list with funceble.
    Former-commit-id: 8ff3d3b73aa9f219b0d57e0caf01613468dc7e38

---
+++ .dev-tools/install-run-funceble.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Fetch funceble script files and run a test
+# Created by: Mitchell Krog (mitchellkrog@gmail.com)
+# Copyright: Mitchell Krog - https://github.com/mitchellkrogza
+# Repo Url: https://github.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites
+
+# ****************************************************************
+# This uses the awesome funceble script created by Nissar Chababy
+# Find funceble at: https://github.com/funilrys/funceble
+# ****************************************************************
+
+# ******************
+# Set our Input File
+# ******************
+_input1=$TRAVIS_BUILD_DIR/.input_sources/combined-list.txt
+
+# *****************************************
+# Get latest versions of funceble from repo
+# *****************************************
+# sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/funceble -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble
+# sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/tool -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool
+# sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/iana-domains-db -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/iana-domains-db
+
+# *********************************
+# Make scripts executable by Travis
+# *********************************
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble
+
+cd $TRAVIS_BUILD_DIR/.dev-tools/_funceble/
+
+# *************************
+# Run Funceble Install Tool
+# *************************
+
+
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool --autosave-minutes 35 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+
+# ************************************
+#  Run Funceble and Check Domains List
+# ************************************
+
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble  --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/generate-hosts.sh && bash $TRAVIS_BUILD_DIR/.dev-tools/modify-readme-file.sh" --travis -a -ex -h -f $_input
+
+exit 0
---
+++ .dev-tools/pre-deploy.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# Duplicate Sorting for the Big List of Hacked Malware Web Sites
+# Created by: Mitchell Krog (mitchellkrog@gmail.com)
+# Copyright: Mitchell Krog - https://github.com/mitchellkrogza
+# Repo Url: https://github.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites
+
+# MIT License
+
+# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
+# https://github.com/mitchellkrogza
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# ******************
+# Set Some Variables
+# ******************
+
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+cd $TRAVIS_BUILD_DIR
+
+# *******************************
+# Remove Remote Added by TravisCI
+# *******************************
+
+git remote rm origin
+
+# **************************
+# Add Remote with Secure Key
+# **************************
+
+git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+
+# **********************************************************************************
+# List Remotes ONLY DURING testing - do not do this on live repo / possible key leak
+# git remote -v
+# ***********************************************************************************
+
+# *********************
+# Set Our Git Variables
+# *********************
+
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
+
+# *******************************************
+# Make sure we have checked out master branch
+# *******************************************
+
+git checkout master
+
+# *************************
+# Generate our testing list
+# *************************
+
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/generate-domain-testing-file.sh
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/generate-domain-testing-file.sh
+
+# ***************************************************
+# Run funceble to check for dead domains
+# ***************************************************
+
+sudo sh -x $TRAVIS_BUILD_DIR/.dev-tools/install-run-funceble.sh
+
+# MIT License
+
+# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
+# https://github.com/mitchellkrogza
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
