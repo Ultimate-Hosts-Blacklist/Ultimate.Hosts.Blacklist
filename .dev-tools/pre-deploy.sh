4a9595ed4b7058bc0f89f59b21e1e60a6511b2c4
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-27 15:12:03 +0200

    Run Funceble Testing (5)
    Former-commit-id: 8dd9e18745e37d9b0d923f80bd8062b8ea2cbf10

--- .dev-tools/pre-deploy.sh
+++ .dev-tools/pre-deploy.sh
@@ -84,6 +84,7 @@ ls -la $TRAVIS_BUILD_DIR/.input_sources/
 # Run funceble to check for dead domains
 # ***************************************************
 
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/install-run-funceble.sh
 sudo bash $TRAVIS_BUILD_DIR/.dev-tools/install-run-funceble.sh
 
 # MIT License
