ce8c291b2a27a73c2efb32ce99887531b0465616
Mitchell Krog <mitchellkrog@gmail.com>
2017-09-11 11:51:31 +0200

    Modify Windows .bat scripts to 1) install 2) update 3) uninstall [funceble skip]
    Former-commit-id: 3ef94cdd1355bb8b09e60d5021e5b66d9a411445
    Former-commit-id: a687ba037c94f92220e62f6581f8aaebc5a54a9e

---
+++ windows-update-hosts.bat
@@ -0,0 +1,40 @@
+:: ULTIMATE HOSTS BLACKLIST
+:: REPO: https://github.com/mitchellkrogza/Ultimate.Hosts.Blacklist
+:: Copyright Mitchell Krog - https://github.com/mitchellkrogza
+:: This script will first create a backup of the original or current hosts
+:: file and save it in a file titled "hosts.bak" 
+:: It will then download the latest updated hosts file
+:: Finally the DNS cache will also be refreshed.
+:: THIS BAT FILE MUST BE LAUNCHED WITH ADMINISTRATOR PRIVILEGES
+:: Copyright Mitchell Krog - https://github.com/mitchellkrogza
+
+:: @ECHO OFF
+TITLE Update Hosts
+
+:: Check if we are administrator. If not, exit immediately.
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %ERRORLEVEL% NEQ 0 (
+    ECHO This script must be run with administrator privileges!
+    ECHO Please launch command prompt as administrator. Exiting...
+    EXIT /B 1
+)
+:: Download Latest Updated Hosts File
+:: Uses a Windows component called BITS 
+:: It has been included in Windows since XP and 2000 SP3
+
+bitsadmin.exe /transfer "Download Fresh Hosts File" https://hosts.ubuntu101.co.za/hosts.windows C:\tmp\hosts
+
+echo Move new hosts file in-place
+
+:: Move new hosts file in-place
+
+COPY C:\tmp\hosts %WINDIR%\System32\drivers\etc\
+
+echo Flush the DNS cache
+
+:: Flush the DNS cache
+
+ipconfig /flushdns
+
+echo ALL DONE !!! Enjoy the Protection !!
