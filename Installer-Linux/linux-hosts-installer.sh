d2a538ba14fde8eba29cea2be84f04151c789f3c
spirillen <44526987+spirillen@users.noreply.github.com>
2020-02-03 23:00:17 +0100

    A few very good suggestions
    Implanted a few very good suggestions from @funilrys :+1:

--- Installer-Linux/linux-hosts-installer.sh
+++ Installer-Linux/linux-hosts-installer.sh
@@ -8,9 +8,9 @@
 #
 # root has to run the script
 #
-if [ `whoami` != "root" ]
+if [[ $(id -u -n) != "root" ]]
     then
-    printf "You need to be root to do this!\nIf you have SUDO installed, then run this script with sudo \$script.bash\n"
+    printf "You need to be root to do this!\nIf you have SUDO installed, then run this script with `sudo ${0}`"
     exit 1
 fi
 
