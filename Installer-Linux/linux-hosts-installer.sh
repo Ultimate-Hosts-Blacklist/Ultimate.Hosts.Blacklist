8d6c85999a95f03ff509cce9548bd9b28a56817d
Nikolas Garofil <nikolas@garofil.be>
2021-02-28 15:14:46 +0100

    Use returncode 127 to check if commands are present

--- Installer-Linux/linux-hosts-installer.sh
+++ Installer-Linux/linux-hosts-installer.sh
@@ -17,22 +17,20 @@ if [ $(id -u) -eq 0 ] ; then
 else
     # Multiple tricks to run this script as root before running the real script
     echo "This script needs to be ran as root, let's switch to root:"
-    sudo=$(which sudo)
-    if [ ! -z $sudo ] ; then
-        $sudo $0 && exit 0
-        echo "Something went wrong with trying to use 'sudo' to run this script"
-        exit 1
-    else
+    sudo $0 && exit 0
+    if [ $? -eq 127 ] ; then
         echo "I can't find 'sudo', I'll try to use 'su' to become root."
-        echo "(Remember that 'su' is deprecated, I strongly recommend installing 'sudo'.)"
-        su=$(which su)
-        if [ ! -z $su ] ; then
-            $su -c $0 && exit 0
-            echo "Something went wrong with trying to use 'su' to run this script"
-            exit 1
-        else
+	echo "(Remember that 'su' is considerd deprecated (it needs a root password), I strongly recommend installing 'sudo'.)"
+        su -c $0 && exit 0
+        if [ $? -eq 127 ] ; then
             echo "I also can't find 'su', you'll need to find another way to become root..."
             exit 1
+        else
+            echo "Something went wrong with trying to use 'su' to run this script"
+            exit 1
         fi
+    else
+        echo "Something went wrong with trying to use 'sudo' to run this script"
+        exit 1
     fi
 fi
